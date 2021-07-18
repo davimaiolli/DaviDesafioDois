9117437edcecb9b559328594f0e499eecc49b349
davimaiolli <davimaiolli@hotmail.com>
2021-07-18 18:20:15 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const formulario = document.getElementById('formulario');
+let nome;
+let email;
+let cadastro;
+let converteCadastro;
+
+formulario.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    nome = document.getElementById('nome').value;
+    email = document.getElementById('email').value;
+    cadastro = {nome, email,}
+    converteCadastro = JSON.stringify(cadastro);
+
+    localStorage.setItem('lead', converteCadastro);
+
+    console.log(nome,email, cadastro);
+    alert("Seja bem Vindo!");
+
+    
+
+})
