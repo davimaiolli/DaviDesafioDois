b13a5b590a8a2aba5216c75d195c1b95c99dcf91
davimaiolli <davimaiolli@hotmail.com>
2021-07-25 23:26:31 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,13 +57,15 @@ formulario.addEventListener('submit', (e)=>{
 formularioAranha.addEventListener('submit', (e)=>{    
     e.preventDefault();
     precoAranha = 300.00;
-    comprouAranha = document.getElementById('compraAranha').value;
-    carrinho = {comprouAranha}
-    converteCadastro = JSON.stringify(carrinho);
-    console.log(carrinho);
-    alert("Você comprou o action figure do Aranha de Ferro.");
     qtdAranha = qtdAranha +1;
     gastouComAranha = precoAranha * qtdAranha;
+    comprouAranha = document.getElementById('compraAranha').value;
+    carrinho = {comprouAranha}
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('Aranhas vendidos', qtdAranha);
+    localStorage.setItem('Compras de Aranhas', gastouComAranha);
+    console.log(carrinho);
+    alert("Você comprou o action figure do Aranha de Ferro.");    
     carrinhoDeCompras ();    
     }
 )
@@ -71,71 +73,84 @@ formularioAranha.addEventListener('submit', (e)=>{
 formularioGroot.addEventListener('submit', (e)=>{
     e.preventDefault();
     precoGroot = 200.00;
-    comprouGroot = document.getElementById('compraGroot').value;
-    carrinho = {comprouGroot}
-    converteCadastro = JSON.stringify(carrinho);
-    console.log(carrinho); 
-    alert("Você comprou o action figure do Baby Groot.");
     qtdGroot = qtdGroot +1; 
     gastouComGroot = precoGroot * qtdGroot;
+    comprouGroot = document.getElementById('compraGroot').value;
+    carrinho = {comprouGroot}
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('Groots vendidos', qtdGroot);
+    localStorage.setItem('Compras de Groots', gastouComGroot);
+    console.log(carrinho); 
+    alert("Você comprou o action figure do Baby Groot.");    
     carrinhoDeCompras ();      
 })
 
 formularioHulk.addEventListener('submit', (e)=>{
     e.preventDefault();
     precoHulk = 350.00;
-    comprouHulk = document.getElementById('compraHulk').value;
-    carrinho = {comprouHulk}
-    converteCadastro = JSON.stringify(carrinho);
-    console.log(carrinho);
-    alert("Você comprou o action figure do Hulk.");
     qtdHulk = qtdHulk +1;
     gastouComHulk = precoHulk * qtdHulk;
+    comprouHulk = document.getElementById('compraHulk').value;
+    carrinho = {comprouHulk}
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('Hulks vendidos', qtdHulk);
+    localStorage.setItem('Compras de Hulks', gastouComHulk);
+    console.log(carrinho);
+    alert("Você comprou o action figure do Hulk.");    
     carrinhoDeCompras ();    
 })
 
 formularioCaneca.addEventListener('submit', (e)=>{
     e.preventDefault();
     precoCaneca = 50.00;
-    comprouCaneca = document.getElementById('compraCaneca').value;
-    carrinho = {comprouCaneca}
-    converteCadastro = JSON.stringify(carrinho);
-    console.log(carrinho);
-    alert("Você comprou a Caneca da Capitã Marvel.");    
     qtdCaneca = qtdCaneca + 1;
     gastouComCaneca = precoCaneca * qtdCaneca;
+    comprouCaneca = document.getElementById('compraCaneca').value;
+    carrinho = {comprouCaneca}
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('Canecas vendidas', qtdCaneca);
+    localStorage.setItem('Compras canecas', gastouComCaneca);
+    console.log(carrinho);
+    alert("Você comprou a Caneca da Capitã Marvel.");    
+    
     carrinhoDeCompras ();
 })
 
 formularioCamisa.addEventListener('submit', (e)=>{
     e.preventDefault();
     precoCamisa = 100.00;
-    comprouCamisa = document.getElementById('compraCamisa').value;
-    carrinho = {comprouCamisa}
-    converteCadastro = JSON.stringify(carrinho);
-    console.log(carrinho);
-    alert("Você comprou a camisa da SheHulk.");
     qtdCamisa = qtdCamisa + 1;
     gastouComCamisa = precoCamisa * qtdCamisa;
+    comprouCamisa = document.getElementById('compraCamisa').value;
+    carrinho = {comprouCamisa}
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('Camisas vendidas', qtdCamisa);
+    localStorage.setItem('Compras Camisas', gastouComCamisa);
+    console.log(carrinho);
+    alert("Você comprou a camisa da SheHulk.");   
     carrinhoDeCompras ();
 })
 
 formularioMiss.addEventListener('submit', (e)=>{
     e.preventDefault();  
-    let precoMiss = 400.00;    
+    let precoMiss = 400.00;   
+    qtdMiss = qtdMiss + 1; 
+    gastouComMiss = precoMiss * qtdMiss;  
     comprouMiss = document.getElementById('compraMiss').value;
     carrinho = {comprouMiss}
-    converteCadastro = JSON.stringify(carrinho);
+    carrinho = JSON.stringify(carrinho);
+    localStorage.setItem('MissMarvel vendidas', qtdMiss);
+    localStorage.setItem('Compras MissMarvel', gastouComMiss);
     console.log(carrinho);
-    alert("Você comprou a action figure da Miss Marvel.");
-    qtdMiss = qtdMiss + 1;  
-    gastouComMiss = precoMiss * qtdMiss;
+    alert("Você comprou a action figure da Miss Marvel.");   
     carrinhoDeCompras ();
 })
 
 function carrinhoDeCompras (){ 
     totalCarrinho = gastouComMiss + gastouComCamisa + gastouComCaneca + gastouComHulk + gastouComGroot + gastouComAranha;
-    console.log(totalCarrinho);    
+    console.log("Total de Compras: R$" + totalCarrinho);
+    totalCarrinho = JSON.stringify(totalCarrinho);
+    localStorage.setItem('Total de Compras', totalCarrinho);    
 }
     
 
